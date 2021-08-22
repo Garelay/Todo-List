f9dd23a472bb5d73e9b4f8ad1b2b2a9f2a06420b
Konstantin Mochalov <constantin.mochalov@gmail.com>
2021-08-22 14:44:08 +0300

    updated styling, added autofocus

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 import {GoDiffAdded} from 'react-icons/go';
+// import { useEffect } from 'react/cjs/react.development';
 
 function Form({todos,setTodos,setFilterStatus,filteredTodos}) {
     const [input, setInput] = useState("")
+    const focusRef = useRef(null)
+    useEffect(()=>{
+        handleClick()
+    },[])
     
     const handleChange = e => {
         setInput(e.target.value)
     }
     const handleSubmit = async (e) =>{
-        e.preventDefault();
-        if (input === ""){
+        e.preventDefault()
+        if (/^\s*$/.test(input)){
             return
         }
         const newTodos = [...todos, {id: uuidv4(), task: input, copleted:false, edit:false}]
-        await setTodos(newTodos);
-        setInput("")        
+        await setTodos(newTodos)
+        setInput("")      
       }
+    const handleClick = () =>{
+        focusRef.current.focus()
+    }
     const handleFilterStatusChange = (e) =>{
         setFilterStatus(e.target.value)
     }
@@ -24,11 +32,10 @@ function Form({todos,setTodos,setFilterStatus,filteredTodos}) {
     return (
         <div className="form-container"> 
             <form className="form" onSubmit={handleSubmit}>
-                <input type="text" className="input" placeholder="Add a task" value={input} onChange={handleChange}/>
-                <button className="add">
+                <input type="text" className="input" placeholder="Add a task" value={input} onChange={handleChange} ref={focusRef}/>
+                <button className="add" onClick={handleClick}>
                     <GoDiffAdded/>
-                </button>
-                
+                </button>                
             </form>
             <select className = "filter-select" onChange={handleFilterStatusChange}>
                 <option value="all">All</option>
